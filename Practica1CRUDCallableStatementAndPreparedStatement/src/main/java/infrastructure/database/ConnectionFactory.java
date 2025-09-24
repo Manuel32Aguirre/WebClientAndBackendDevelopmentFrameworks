7d7c81e891deab58b4ea3d964956cb0a8d09b153
vic-s <programadorjrsemad@gmail.com>
2025-09-23 23:05:26 -0600

    Practica 1 implementación de db.properties

--- Practica1CRUDCallableStatementAndPreparedStatement/src/main/java/infrastructure/database/ConnectionFactory.java
+++ Practica1CRUDCallableStatementAndPreparedStatement/src/main/java/infrastructure/database/ConnectionFactory.java
@@ -1,16 +1,34 @@
 package infrastructure.database;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionFactory {
-    private static final String URL = "jdbc:mysql://localhost:3307/7CM1_26_1";
-    private static final String USER = "manuel";
-    private static final String PASS = "1234";
+    private static final Properties props = new Properties();
+    static{
+    try(InputStream in = ConnectionFactory.class.getClassLoader().getResourceAsStream("db.properties")){
+        if(in == null){
+            throw new RuntimeException("No se encontró el archivo db.properties");
+        }
+        props.load(in);
+
+        Class.forName(props.getProperty("db.driver"));
+    }catch(IOException | ClassNotFoundException e){
+        throw new RuntimeException("Error cargando configuración de DB" + e);
+    }
+}
+
 
     public static Connection get() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASS);
+        return DriverManager.getConnection(
+                props.getProperty("db.url"),
+                props.getProperty("db.user"),
+                props.getProperty("db.password")
+        );
     }
 
 }
